a2e68a45a2554f1c77566595e12c4a85fdefc505
welp <welperooni@gmail.com>
2019-12-01 11:45:17 +0100

    day 1 cleanup

--- src/Ex/E1.hs
+++ src/Ex/E1.hs
@@ -1,7 +1,10 @@
 module Ex.E1 (solve1p1, solve1p2) where
 
-solve1p1 = print . sum . map (subtract 2 . floor . (/3) . read) . lines
+solve1p1 = print . sum . map (subtract 2 . (`div` 3) . read) . lines
 solve1p2 = print . sum
   . map (sum . takeWhile (>= 0) . tail . iterate f . read) . lines
   where
-    f = subtract 2 . floor . (/3) . fromIntegral
+    f = subtract 2 . (`div` 3)
+
+
+
